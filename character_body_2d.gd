6da61322eeb0f5eb1580193d38a46b920965f515
animedad420 <guildedfalcon@hotmail.com>
2025-06-03 15:22:56 +1000

    physics changes

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,26 +5,44 @@ const SPEED = 30.0
 const JUMP_VELOCITY = -20.0
 var closestPlanet : Node
 var x_vel : float = 0
+var y_vel : float = 0
+
+
+func _ready() -> void:
+	set_motion_mode(CharacterBody2D.MOTION_MODE_FLOATING)
+	velocity.y = 135
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_wall():
 		#velocity += get_gravity() * delta
 		#print($Area2D.get_overlapping_areas())
 		var a = $Area2D.get_overlapping_areas()
 		var closeDist: float = 99999
 		for i in a.size():
 			#print(a[i])
-			var b = global_position.distance_to(a[i].global_position)
+			var pull = a[i].get_parent().pull
+			var size = a[i].get_parent().size
+			var gravity = a[i].get_parent().gravity
+			var planetPos = a[i].global_position
+			
+			var b = global_position.distance_to(planetPos)
 			if b < closeDist:
 				closeDist = b
 				closestPlanet = a[i].get_parent()
-			print(b)
-			print(b-a[i].get_parent().size)
-			var c = global_position.direction_to(a[i].global_position)
-			print(c)
-			velocity += c*Vector2(b-a[i].get_parent().size,b-a[i].get_parent().size) * delta
-			print(c*Vector2(b-a[i].get_parent().size,b-a[i].get_parent().size))
+			#print(b)
+			#print(b-a[i].get_parent().size)
+			var c = global_position.direction_to(planetPos)
+			#print(c)
+			var d = size + (gravity*pull*size*5)
+			print(d-b)
+			var e = d-(b/pull)
+			if e < 0:
+				e = 0.1
+			velocity += c*Vector2(e,e) * delta
+			#print(c*Vector2(b-a[i].get_parent().size,b-a[i].get_parent().size))
+			#print(c*Vector2(d-a[i].get_parent().size,d-a[i].get_parent().size))
 	else:
 		velocity = Vector2.ZERO
 
@@ -33,21 +51,35 @@ func _physics_process(delta: float) -> void:
 	#print(rotation)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_wall():
 		var a = global_position.direction_to(closestPlanet.global_position)
 		velocity += a * Vector2(JUMP_VELOCITY,JUMP_VELOCITY)
 		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		x_vel = direction * SPEED
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		x_vel = direction_x * SPEED
 	else:
 		x_vel = 0
 	
-	velocity.x += x_vel
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		y_vel = direction_y * SPEED
+	else:
+		y_vel = 0
+	
+	var mouse = get_viewport().get_mouse_position()
+	var point = global_position.direction_to(mouse)
+	#print(point)
+	if Input.is_action_pressed("click"):
+		velocity += 50*point*delta
+	
+	#velocity.x += x_vel
+	velocity += Vector2(x_vel,y_vel)
 
 	move_and_slide()
 	
-	velocity.x -= x_vel
+	#velocity.x -= x_vel
+	velocity -= Vector2(x_vel,y_vel)
--- planet.gd
+++ planet.gd
@@ -6,9 +6,15 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
 	$CollisionShape2D.shape.radius = size
 	#$Panel.size = Vector2(size,size)
+	$Area2D/CollisionShape2D.shape = $Area2D/CollisionShape2D.shape.duplicate()
 	$Area2D/CollisionShape2D.shape.radius = size + (pull*size*5)
+	$TextureRect.size = Vector2($Area2D/CollisionShape2D.shape.radius*2,$Area2D/CollisionShape2D.shape.radius*2)
+	$TextureRect.position = Vector2(-$Area2D/CollisionShape2D.shape.radius,-$Area2D/CollisionShape2D.shape.radius)
+	$PlanetPanel.size = Vector2(size*2,size*2)
+	$PlanetPanel.position = Vector2(-size,-size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
