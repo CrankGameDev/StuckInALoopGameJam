42e962a6e9b38099632ec93c6ad343550af32da9
animedad420 <guildedfalcon@hotmail.com>
2025-06-06 12:58:11 +1000

    nicer gradients

--- planet.gd
+++ planet.gd
@@ -11,8 +11,30 @@ func _ready() -> void:
 	#$Panel.size = Vector2(size,size)
 	$Area2D/CollisionShape2D.shape = $Area2D/CollisionShape2D.shape.duplicate()
 	$Area2D/CollisionShape2D.shape.radius = size + (pull*size*5)
-	$TextureRect.size = Vector2($Area2D/CollisionShape2D.shape.radius*2,$Area2D/CollisionShape2D.shape.radius*2)
-	$TextureRect.position = Vector2(-$Area2D/CollisionShape2D.shape.radius,-$Area2D/CollisionShape2D.shape.radius)
+	
+	print($Area2D/CollisionShape2D.shape.radius)
+	var diff:float = ($Area2D/CollisionShape2D.shape.radius-size)/$Area2D/CollisionShape2D.shape.radius
+	print(diff)
+	var rev:float = 1-diff
+	print(rev)
+	
+	$TextureRect.texture = $TextureRect.texture.duplicate()
+	$TextureRect.texture.width =$Area2D/CollisionShape2D.shape.radius*2
+	$TextureRect.texture.height =$Area2D/CollisionShape2D.shape.radius*2
+	var grad = Gradient.new()
+	grad.remove_point(1)
+	grad.add_point(0,Color(1,0,0,1))
+	grad.add_point(rev-0.01,Color(0,0,0,1))
+	grad.add_point(rev+0.01,Color(0,0,0,0))
+	grad.add_point(0.27,Color(0,0,0,0))
+	grad.add_point(0.3,Color(1,1,1,1))
+	grad.add_point(0.33,Color(0,0,0,0))
+	grad.add_point(0.6,Color(1,1,1,0))
+	grad.add_point(0.7,Color(1,0,0,0.5))
+	grad.add_point(0.701,Color(1,0.18,0.18,0))
+	#print(grad.get_point_count())
+	$TextureRect.texture.gradient = grad
+	#$TextureRect.position = Vector2($Area2D/CollisionShape2D.shape.radius,$Area2D/CollisionShape2D.shape.radius)
 	$PlanetPanel.size = Vector2(size*2,size*2)
 	$PlanetPanel.position = Vector2(-size,-size)
 
