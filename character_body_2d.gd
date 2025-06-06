fcfdad081991c6c9e29d1cdc8e7909c7474c2acc
animedad420 <guildedfalcon@hotmail.com>
2025-06-06 13:00:38 +1000

    camera (and gravity debug label)

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,6 +7,7 @@ var closestPlanet : Node
 var x_vel : float = 0
 var y_vel : float = 0
 
+var DEBUG : float = 0
 
 func _ready() -> void:
 	set_motion_mode(CharacterBody2D.MOTION_MODE_FLOATING)
@@ -38,6 +39,7 @@ func _physics_process(delta: float) -> void:
 			var d = size + (gravity*pull*size*5)
 			print(d-b)
 			var e = d-(b/pull)
+			DEBUG = e
 			if e < 0:
 				e = 0.1
 			velocity += c*Vector2(e,e) * delta
@@ -70,11 +72,22 @@ func _physics_process(delta: float) -> void:
 	else:
 		y_vel = 0
 	
-	var mouse = get_viewport().get_mouse_position()
-	var point = global_position.direction_to(mouse)
+	#var mouse = get_viewport().get_mouse_position()
+	#var size:Vector2 = DisplayServer.screen_get_size()/2
+	#var point = global_position.direction_to(mouse)
+	#print(get_local_mouse_position())
+	
+	var point = get_local_mouse_position().normalized()
+	
 	#print(point)
 	if Input.is_action_pressed("click"):
 		velocity += 50*point*delta
+		$LineThrust.points[1] = -point*20
+	else:
+		$LineThrust.points[1] = Vector2.ZERO
+	
+	$LineVelocity.points[1] = velocity
+	#$LineThrust.points[1] = point*20
 	
 	#velocity.x += x_vel
 	velocity += Vector2(x_vel,y_vel)
--- scene.gd
+++ scene.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+@export var curve: Curve
+var buf: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,3 +15,18 @@ func _physics_process(delta: float) -> void:
 	while $Line2D.get_point_count() > 1000:
 		#print($Line2D.get_point_count())
 		$Line2D.remove_point(0)
+	$Camera2D/Label.text = str($CharacterBody2D.DEBUG)
+
+
+func _process(delta: float) -> void:
+	$Camera2D.global_position = $CharacterBody2D.global_position
+	var zoomed = $CharacterBody2D.velocity.length()
+	zoomed = remap(zoomed,150,30,0.5,1.5)
+	#zoomed = clampf(zoomed,0.5,1.5)
+	zoomed = curve.sample(zoomed)
+	if buf != zoomed:
+		print(zoomed)
+	buf = zoomed
+	
+	#zoomed = maxf(zoomed,1)
+	$Camera2D.zoom = Vector2(zoomed,zoomed)
