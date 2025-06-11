d48aacc3b988bfd734a7554dfd94c5d33bbac40b
Hamish Bell <hamish@lucid.com.au>
2025-06-11 10:41:20 +1000

    new: Added 'time_limit' to 'level.gd'
    - Adds a 'time_limit' property to Level to set the amount of time allowed for completion
    - Adds an internal Timer child to Level to track the time left in a level
    - Adds a 'timeout' signal to Level, emitted when the 'time_limit' is exhausted

--- scripts/scenes/level.gd
+++ scripts/scenes/level.gd
@@ -1,12 +1,42 @@
+## Represents a level in the game.
 class_name Level
 extends Node
+
+## A signal emitted when the time limit of this level is elapsed.
+signal timeout
 
 @onready var player: CharacterBody2D = %Player
 @onready var line:  Line2D = %Line
 @onready var camera: Camera2D = %Camera
 
+@onready var time_left_label: Label = %TimeLeftLabel
+
+## The amount of time for the level to be completed in. [br]
+## If the player persists in the level for longer than this amount of time
+## then the [signal timeout] signal will be emitted.
+@export_range(0.0, 600.0, 0.5, "or_greater", "suffix:seconds")
+var time_limit: float = 300.0
+
 @export var curve: Curve
+
 var buf: float
+
+## The amount of time left for level completion. [br]
+## Returns [code]-1.0[/code] if the level is not yet ready.
+var time_left: float :
+	get: return _level_timer.time_left if _level_timer else -1.0
+
+var _level_timer: Timer
+
+
+func _ready() -> void:
+	# Create an internal Timer node to track the level time.
+	_level_timer = Timer.new()
+	_level_timer.autostart = true
+	_level_timer.one_shot = true
+	_level_timer.wait_time = time_limit
+	_level_timer.timeout.connect(self.timeout.emit)
+	add_child(_level_timer, false, Node.INTERNAL_MODE_FRONT)
 
 
 func _physics_process(delta: float) -> void:
@@ -27,3 +57,11 @@ func _process(delta: float) -> void:
 	buf = zoomed
 	#zoomed = maxf(zoomed,1)
 	camera.zoom = Vector2(zoomed,zoomed)
+
+	# Display time left in whole minutes and seconds.
+	var minutes_left: int = floori(_level_timer.time_left / 60.0)
+	var seconds_left: int = floori(_level_timer.time_left) % 60
+	time_left_label.text = "{mins}:{secs}".format({
+		"mins": minutes_left,
+		"secs": "%02d" % seconds_left,
+	})
