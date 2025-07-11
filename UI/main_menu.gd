41a1f4508ff1497d14ac76b1e4d601cfcb809d03
animedad420 <guildedfalcon@hotmail.com>
2025-07-11 11:50:23 +1000

    tied main menu to first level

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene.tscn")
+	get_tree().change_scene_to_file("res://levels/demo_level.tscn")
 	
 func _on_exit_pressed() -> void:
 	get_tree().quit() 
